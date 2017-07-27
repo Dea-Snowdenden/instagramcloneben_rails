f0233f72250293987498dab93d08491ce6ac287e
Dea Lorenzo <deazee101@gmail.com>
2017-07-27 08:51:42 +0000

    first commit, cloned copy from Ben

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,24 @@
+class PostsController < ApplicationController
+    
+    def index  
+    end  
+    
+    def new  
+    @post = Post.new
+    end 
+    
+    def create  
+    @post = Post.create(post_params)
+    redirect_to posts_path  
+    end 
+    
+    def index  
+    @posts = Post.all
+    end  
+    
+    private
+    def post_params  
+    params.require(:post).permit(:image, :caption)
+    end
+    
+end
